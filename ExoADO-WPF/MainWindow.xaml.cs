bffb979d4e81bf3f0ddaa8de72de6018fd2f529e
Alexandre Engelhardt <alexandre@zipion-live.be>
2013-02-05 17:50:52 +0100

    Work in progress...
    La connection fonctionne, l'interface de requête reste à achever

---
+++ ExoADO-WPF/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ToolBox;
+
+namespace ExoADO_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ExploreWindow wExplore;
+        private bool connected = false;
+        private SqlConnection db;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            if (!connected)
+            {
+                Dictionary<string, string> credentials = new Dictionary<string, string>();
+
+                credentials.Add("login", tbLogin.Text);
+                credentials.Add("password", pbPassword.Password);
+
+                tbLogin.Text = "";
+                pbPassword.Password = "";
+
+                DBConnector dbc = new DBConnector(credentials);
+                db = dbc.CreateDBConnect();
+
+                try
+                {
+                    db.Open();
+                    connected = true;
+                    if (db.State.ToString().Equals("Open"))
+                        lbConnectionStatus.Content = "Online";
+                    ConnectionStatusBlock.Background = Brushes.Green;
+                    btnConnect.Content = "Disconnect";
+
+                    wExplore = new ExploreWindow();
+                    wExplore.Title = "DataBase Reader - Connected as " + dbc.login; 
+                    wExplore.Show();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erreur de connection à la DB\n" + ex.Message);
+                }
+            }
+            else
+            {
+                db.Close();
+                connected = false;
+                btnConnect.Content = "Connect !";
+                if (db.State.ToString().Equals("Closed"))
+                    lbConnectionStatus.Content = "Offline";
+                ConnectionStatusBlock.Background = Brushes.DarkRed;
+
+                wExplore.Close();
+            }
+        }
+    }
+}
---
+++ ToolBox/DBConnector.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace ToolBox
+{
+    public class DBConnector
+    {
+        public string login { get; private set; }
+        private string pwd;
+
+        public DBConnector (Dictionary<string, string> connectParams)
+        {
+            foreach (KeyValuePair<string, string> kvp in connectParams)
+            {
+                if (kvp.Key.Equals("login"))
+                    this.login = kvp.Value;
+                else if (kvp.Key.Equals("password"))
+                    this.pwd = kvp.Value;
+            }
+        }
+
+        public SqlConnection CreateDBConnect()
+        {
+            //SqlParameter sqlpLogin = new SqlParameter();
+            //sqlpLogin.ParameterName = "@login";
+            //sqlpLogin.Value = this.login;
+
+            //SqlParameter sqlpPassword = new SqlParameter();
+            //sqlpPassword.ParameterName = "@password";
+            //sqlpPassword.Value = pwd;
+
+            StringBuilder csBuild = new StringBuilder();
+            csBuild.Append(@"Data Source=DOTNET703; Initial Catalog=AdventureWorks2008R2; User Id=");
+            csBuild.Append(this.login);
+            csBuild.Append("; Password=");
+            csBuild.Append(this.pwd);
+            csBuild.Append(";");
+            string connectString = csBuild.ToString();
+            //string connectString = (@"Data Source = ASPIRE-5560G\ZLSQL; Initial Catalog = DBSlides; User Id = @login; Password = @password;");
+            SqlConnection db = new SqlConnection();
+            db.ConnectionString = connectString;
+
+            return db;
+        }
+    }
+}
